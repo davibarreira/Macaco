5c4cbc170c08e064ee0802810cf00664a8004281
davisbarreira <davisbarreira@gmail.com>
2019-09-05 23:32:18 -0300

    Module Macaco.py com funcionalidade de inserir o DataFrame

--- PythonBoost/Macaco.py
+++ PythonBoost/Macaco.py
@@ -2,8 +2,12 @@ from DataFrame import *
 
 class DataFrameMc():
     """docstring for DataFrameMc"""
-    def __init__(self):
+    def __init__(self,dados=None):
         self.df = DataFrame()
+        if dados != None:
+            for coluna in dados:
+                self.InserirColuna(dados[coluna],coluna)
+
 
     def InserirColuna(self,valores,nome_coluna):
         # Infere o tipo do dado. Caso encontre multiplos,
@@ -12,8 +16,11 @@ class DataFrameMc():
         if all(isinstance(x, int) for x in valores):
             self.df.InserirColunaInt(valores,nome_coluna)
 
-        if all(isinstance(x, (float,int)) for x in valores):
+        elif all(isinstance(x, (float,int)) for x in valores):
             self.df.InserirColunaDouble(valores,nome_coluna)
 
-        else all(isinstance(x, str) for x in valores):
+        elif all(isinstance(x, str) for x in valores):
             self.df.InserirColunaString(valores,nome_coluna)
+        else:
+            valores_string = [str(i) for i in list(valores)]
+            self.df.InserirColunaString(valores_string,nome_coluna)
---
+++ PythonBoost/test2.py
@@ -0,0 +1,10 @@
+import Macaco as mc
+
+df = mc.DataFrameMc()
+
+df.InserirColuna([5,42,1],"Coluna1")
+df = mc.DataFrameMc()
+
+data = {'col_1': [3, 2, 1, 0], 'col_2': ['a', 'b', 'c', 'd']}
+df = mc.DataFrameMc(data)
+

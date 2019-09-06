ff54366304c1ba56c1f9356bc105f08e65eade3f
davisbarreira <davisbarreira@gmail.com>
2019-09-06 10:36:56 -0300

    Implementada funcao de GetColuna e exception para numero de colunas de inteiros

--- PythonBoost/Macaco.py
+++ PythonBoost/Macaco.py
@@ -3,8 +3,10 @@ from DataFrame import *
 class DataFrameMc():
     """docstring for DataFrameMc"""
     def __init__(self,dados=None):
-        self.df = DataFrame()
-        self.colunas = []
+        self.df      = DataFrame()
+        self.colunas = {}
+        self.shape   = (0,0) # Quantidade de linhas x colunas
+
         if dados != None:
             for coluna in dados:
                 self.InserirColuna(dados[coluna],coluna)
@@ -15,22 +17,43 @@ class DataFrameMc():
         # transforma em string
 
         if all(isinstance(x, int) for x in valores):
-            self.df.InserirColunaInt(valores,nome_coluna)
-            self.colunas.append((nome_coluna,'int'))
+            if self.shape == [0,0]:
+                self.df.InserirColunaInt(valores,nome_coluna)
+                self.colunas[nome_coluna] ='int'
+                self.shape[0] = len(valores)
+                self.shape[1] += 1
+            elif self.shape[0] != len(valores):
+                raise Exception("Coluna com tamanhos diferentes.")
+            else:
+                self.df.InserirColunaInt(valores,nome_coluna)
+                self.colunas[nome_coluna] ='int'
+                self.shape[1] += 1
+
 
         elif all(isinstance(x, (float,int)) for x in valores):
             self.df.InserirColunaDouble(valores,nome_coluna)
-            self.colunas.append((nome_coluna,'double'))
+            self.colunas[nome_coluna] ='double'
 
         elif all(isinstance(x, str) for x in valores):
             self.df.InserirColunaString(valores,nome_coluna)
-            self.colunas.append((nome_coluna,'string'))
+            self.colunas[nome_coluna] ='string'
         else:
             valores_string = [str(i) for i in list(valores)]
             self.df.InserirColunaString(valores_string,nome_coluna)
-            self.colunas.append((nome_coluna,'string'))
+            self.colunas[nome_coluna] ='string'
+
+    def GetColuna(self,nome_coluna):
+        if self.colunas[nome_coluna] == 'int':
+            return self.df.GetColunaInt(nome_coluna)
+        elif self.colunas[nome_coluna] == 'double':
+            return self.df.GetColunaDouble(nome_coluna)
+        else:
+            return self.df.GetColunaString(nome_coluna)
+
+
 
 
     # def GetLinha(linha)
     # def GetColuna(coluna)
     # def GetLoc(linha,coluna)
+    # def Query()

d114d9a1b6107777d382967818d2028034f82209
davisbarreira <davisbarreira@gmail.com>
2019-09-05 23:38:46 -0300

    Adicionado atributo de colunas para controlar a entrada e saida de dados

--- PythonBoost/Macaco.py
+++ PythonBoost/Macaco.py
@@ -4,6 +4,7 @@ class DataFrameMc():
     """docstring for DataFrameMc"""
     def __init__(self,dados=None):
         self.df = DataFrame()
+        self.colunas = []
         if dados != None:
             for coluna in dados:
                 self.InserirColuna(dados[coluna],coluna)
@@ -15,12 +16,21 @@ class DataFrameMc():
 
         if all(isinstance(x, int) for x in valores):
             self.df.InserirColunaInt(valores,nome_coluna)
+            self.colunas.append((nome_coluna,'int'))
 
         elif all(isinstance(x, (float,int)) for x in valores):
             self.df.InserirColunaDouble(valores,nome_coluna)
+            self.colunas.append((nome_coluna,'double'))
 
         elif all(isinstance(x, str) for x in valores):
             self.df.InserirColunaString(valores,nome_coluna)
+            self.colunas.append((nome_coluna,'string'))
         else:
             valores_string = [str(i) for i in list(valores)]
             self.df.InserirColunaString(valores_string,nome_coluna)
+            self.colunas.append((nome_coluna,'string'))
+
+
+    # def GetLinha(linha)
+    # def GetColuna(coluna)
+    # def GetLoc(linha,coluna)

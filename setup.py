a76e873cef9a1f0cc6118bde879f9260120255b6
davisbarreira <davisbarreira@gmail.com>
2019-09-16 12:04:31 -0300

    Arquivo de setup.py para instalacao via pip

---
+++ setup.py
@@ -0,0 +1,8 @@
+import setuptools
+
+setuptools.setup(
+        name = 'Macaco',
+        verision = '0.0.1',
+        description = 'My first package',
+        packages = setuptools.find_packages(),
+    )
